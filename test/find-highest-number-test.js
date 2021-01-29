ab7e042247c7e4eb31bd6c904add4b8b5609eaa2
Daniel Frost <daniel.frost@sky.uk>
2021-01-29 16:35:11 +0000

    First step passed

--- test/find-highest-number-test.js
+++ test/find-highest-number-test.js
@@ -2,14 +2,29 @@ var assert = require("chai").assert;
 var expect = require("chai").expect;
 
 // Put your CUT here
+class SortList 
+{
+    findHighestNumber( numberArray )
+    {
+        var result = numberArray[0];
+
+        return result;
+    }
+}
 
 describe('find the highest number', function() {
     it('single item in array, return that item', function() {
         // arrange...
-
+        var cut = new SortList();
+        var expectedResult = 1;
+        var singleElement = [1];
 
         // act...
+        var actualResult = cut.findHighestNumber(singleElement);
 
         // assert...
+        expect(actualResult).to.equal(expectedResult);
+
     });
+    it()
 });
